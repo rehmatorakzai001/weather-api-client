11cbdb426634817d70faa9fb8ec3a147969f28b1
Rehmat Ullah <rehmat.orakzai001@gmail.com>
2026-07-19 03:45:26 -0700

    Update log file path to relative location

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ import logging
 
 def logger(): 
     logging.basicConfig(
-        filename="C:\\Users\\DELL\\Desktop\\Python Projects\\weather_client\\log_database.log",
+        filename="log_database.log",
         level=logging.INFO,
         format="%(asctime)s - [%(levelname)s] - %(message)s",
         datefmt="%Y-%m-%d %H-%M-%S"
